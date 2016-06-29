42388628e510a102cc0935b0a693deb4e11c8d6b
mukehuakai <zhengfeng_sole@163.com>
2016-06-29 09:49:04 +0800

    大数相加功能实现

--- BigData/BigData/BigData.h
+++ BigData/BigData/BigData.h
@@ -15,7 +15,7 @@ class BigData
 public:
 	BigData()
 		:_data(0)
-		, s_data('\0')
+		, s_data("0")
 	{}
 	BigData(INT64 data)
 		:_data(data)
@@ -87,17 +87,17 @@ public:
 					return BigData(_data + bigdata._data);
 				}
 			}
-			string strRet;
-			if (s_data[0] == bigdata.s_data[0])
-			{
-				strRet = Add(s_data, bigdata.s_data);
-			}
-			else
-			{
-				strRet = Sub(s_data, bigdata.s_data);
-			}
-			return BigData(strRet.c_str());
 		}
+		string strRet;
+		if (s_data[0] == bigdata.s_data[0])
+		{
+			strRet = Add(s_data, bigdata.s_data);
+		}
+		else
+		{
+			strRet = Sub(s_data, bigdata.s_data);
+		}
+		return BigData(strRet.c_str());
 	}
 	BigData& operator-(const BigData& bigdata);
 	BigData& operator*(const BigData& bigdata);
@@ -128,9 +128,90 @@ public:
 		}
 	}
 private:
-	string Add(string left, string right);
-	string Sub(string left, string right);
-	void INT64ToStirng();
+	string Add(string left, string right)
+	{
+		int L_Size = left.size();
+		int R_Size = right.size();
+		if (L_Size < R_Size)//保证大的数在left的位置
+		{
+			swap(L_Size, R_Size);
+			swap(left, right);
+		}
+		string strRet;
+		char Step;
+
+		strRet.resize(L_Size + 1);//保证可以保存相加之后的字符串
+		strRet[0] = left[0];
+		Step = 0;                 //进位
+		for (int index = 1; index < L_Size; ++index)//第一位符号位保留，左对齐相加
+		{
+			char Ret = left[L_Size - index] - '0' + Step;
+
+			if (index < R_Size)
+			{
+				Ret += (right[R_Size - index] - '0');
+			}
+			strRet[L_Size - index + 1] = (Ret % 10 + '0');
+			Step = Ret / 10;
+		}
+		strRet[1] = (Step + '0');
+
+		return strRet;
+	}
+	string Sub(string left, string right)
+	{
+		int L_Size = left.size();
+		int R_Size = right.size();
+		if (L_Size < R_Size || (L_Size == R_Size && left < right))
+		{
+			swap(L_Size, R_Size);
+			std::swap(left, right);
+		}
+		string strRet;
+
+		strRet.resize(L_Size);
+		strRet[0] = left[0];
+		for (int index = 1; index < L_Size; index++)
+		{
+			char cRet = left[L_Size - index] - '0';
+			if (index < R_Size)
+			{
+				cRet += (right[R_Size - index] - '0');
+			}
+			if (cRet < 0)
+			{
+				left[L_Size - index - 1] -= 1;
+				cRet += 10;
+			}
+			strRet[L_Size - index] = (cRet + '0');
+		}
+		return strRet;
+	}
+	void INT64ToStirng()
+	{
+		char symbol = '+';
+		INT64 tmp = _data;
+		if (tmp < 0)
+		{
+			symbol = '-';
+			tmp = 0 - tmp;
+		}
+		s_data.append(1, symbol);
+		while (tmp >0)
+		{
+			s_data.append(1, tmp % 10 + '0');
+			tmp = tmp / 10;
+		}
+		char* left = (char*)(s_data.c_str() + 1);
+		char* right = (char*)(s_data.c_str() + s_data.size() - 1);
+		while (left < right)
+		{
+			char ctmp = *left;
+			*left++ = *right;
+			*right-- = ctmp;
+		}
+
+	}
 	bool IsLeftStrBig(char *pLeft, size_t LSize, char *pRight, size_t RSize);
 private:
 	INT64 _data;
