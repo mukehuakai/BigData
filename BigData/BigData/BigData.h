a32133806120a2f1b45833a93a2709c28cdbde92
mukehuakai <zhengfeng_sole@163.com>
2016-07-06 06:44:40 +0800

    Revert "大数运算"
    This reverts commit cd2782d3a86569faa61751ede24a38a7ff35506b.

--- BigData/BigData/BigData.h
+++ BigData/BigData/BigData.h
@@ -1,9 +1,10 @@
 #pragma once
-#include <cassert>
-#include <string>
-#include <iostream>
 
-#define UN_INIT 0xcccccccccccccccc
+#include<assert.h>
+#include<string>
+#include<iostream>
+using namespace std;
+
 #define MAX_INT64 0x7fffffffffffffff
 #define MIN_INT64 0x8000000000000000
 
@@ -12,498 +13,207 @@ typedef long long INT64;
 class BigData
 {
 public:
+	BigData()
+		:_data(0)
+		, s_data("0")
+	{}
 	BigData(INT64 data)
-		: m_llValue(data)
-		, m_strData("")
+		:_data(data)
+		, s_data("")
 	{
-		INT64ToString();
+		INT64ToStirng();
 	}
-	BigData(const char *_pData)
+	BigData(const char* pData)
 	{
-		//1234567 00234567 asdfg12345 123456asdfgh -1234567
-		assert(NULL != _pData);
-
-		char symbol = _pData[0];
-		char* pData = (char*)_pData;
-		if ('+' == symbol || '-' == symbol)
+		assert(pData);
+		char symbol;
+		if (*pData == '+' || *pData == '-')
 		{
+			symbol = *pData;
 			pData++;
 		}
-		else if (*pData >= '0' && *pData <= '9')
+		else if (*pData <= '9' && *pData >= '0')
 		{
 			symbol = '+';
 		}
 		else
 		{
-			m_llValue = 0;
-			m_strData = "0";
+			_data = 0;
+			s_data = "0";
 			return;
 		}
-
-		// 去掉前置0
-		while ('0' == *pData)
-			pData++;
-
-		// "12457aaa123"
-		m_strData.resize(strlen(pData) + 1);
-		m_llValue = 0;
-		m_strData[0] = symbol;
-		int count = 1;
-		while (pData)
+		while (*pData == '0')
 		{
-			if (*pData >= '0' && *pData <= '9')
+			pData++;
+		}
+		_data = 0;
+		s_data.resize(strlen(pData) + 2);
+		int index = 0;
+		s_data[index++] = symbol;
+		while (*pData != '\0')
+		{
+			if (*pData <= '9'&&*pData >= '0')
 			{
-				m_llValue = m_llValue * 10 + *pData - '0';
-				m_strData[count++] = *pData++;
+				_data = _data * 10 + *pData - '0';
+				s_data[index++] = *pData;
+				pData++;
 			}
 			else
 			{
 				break;
 			}
+			
 		}
-
-		m_strData.resize(count);
-
-		if ('-' == symbol)
+		s_data[index] = '\0';
+		if (symbol == '-')
 		{
-			m_llValue = 0 - m_llValue;
+			_data = 0 - _data;
 		}
 	}
 
-	BigData operator+(BigData& bigData);
-	BigData operator-(const BigData& bigData);
-	BigData operator*(const BigData& bigData);
-	BigData operator/(const BigData& bigData);
-	BigData operator%(const BigData& bigData);
-
-	//=======================================
-	bool operator<(const BigData& bigData);
-	bool operator>(const BigData& bigData);
-	bool operator==(const BigData& bigData);
-	
-	std::ostream& operator<<(std::ostream& _cout);
-
-	friend std::ostream& operator<<(std::ostream& _cout, const BigData& bigData);
-	friend std::istream& operator>>(std::istream& _cin, BigData bigData);
-	
-private:
-	std::string Add(std::string left, std::string right)
+	BigData operator+(const BigData& bigdata)
 	{
-		int LSize = left.size();
-		int RSize = right.size();
-
-		if (LSize < RSize)
+		if (!IsINT64Owerflow() && !bigdata.IsINT64Owerflow())
 		{
-			std::swap(left, right);
-			std::swap(LSize, RSize);
-		}
-
-		std::string strRet;
-		strRet.resize(LSize + 1);
-		strRet[0] = left[0];
-		char step = 0;
- 
-		for (int index = 1; index < LSize; ++index)
-		{
-			char cRet = left[LSize - index] - '0' + step;
-
-			if (index < RSize)
+			if (s_data[0] != bigdata.s_data[0])//不同号
 			{
-				cRet += (right[RSize - index] - '0');
-			}
-
-			strRet[LSize - index + 1] = (cRet % 10 + '0');
-			step = cRet / 10;
-		}
-
-		strRet[1] = (step + '0');
-
-		return strRet;
-	}
-	std::string Sub(std::string left, std::string right)
-	{
-		//left > right ? left:right
-		int LSize = left.size();
-		int RSize = right.size();
-		if (LSize < RSize || (LSize == RSize && left < right))
-		{
-			std::swap(left, right);
-			std::swap(LSize, RSize);
-		}
-		char symbol = left[0];
-
-		std::string strRet;
-		strRet.resize(LSize);
-		strRet[0] = symbol;
-
-		// 111111111111
-		// -   11111112
-		// 111099999999
-		for (int index = 1; index < LSize; index++)
-		{
-			char cRet = left[LSize - index] - '0';
-			if (index < RSize)
-			{
-				cRet -= (right[RSize - index] - '0');
-			}
-
-			if (cRet < 0)
-			{
-				left[LSize - index - 1] -= 1;
-				cRet += 10;
-			}
-
-			strRet[LSize - index] = (cRet + '0');
-		}
-
-		return strRet;
-	}
-
-	std::string Mul(std::string left, std::string right)
-	{
-		int LSize = left.size();
-		int RSize = right.size();
-		if (LSize > RSize)
-		{
-			std::swap(left, right);
-			std::swap(LSize, RSize);
-		}
-
-		char symbol = '+';
-		if (left[0] != right[0])
-		{
-			symbol = '-';
-		}
-
-		std::string strRet;
-		strRet.assign(LSize + RSize - 1, '0');
-		strRet[0] = symbol;
-		int DataLen = strRet.size();
-		int Offset = 0;
-
-		for (int index = 1; index < LSize; ++index)
-		{
-			char Left = left[LSize - index] - '0';
-			char step = 0;
-			if (0 == Left)
-			{
-				Offset++;
-				continue;
-			}
-
-			for (int iRIdx = 1; iRIdx < RSize; ++iRIdx)
-			{
-				char cRet = Left*(right[RSize - iRIdx] - '0');
-				cRet += step;
-				cRet += (strRet[DataLen - Offset - iRIdx] - '0');
-				strRet[DataLen - Offset - iRIdx] = cRet % 10 + '0';
-				step = cRet / 10;
-			}
-
-			strRet[DataLen - Offset - RSize] += step;
-			Offset++;
-		}
-
-		return strRet;
-	}
-
-	std::string Div(std::string left, std::string right)
-	{
-		char symbol = '+';
-		if (left[0] != right[0])
-		{
-			symbol = '-';
-		}
-
-		int LSize = left.size();
-		int RSize = right.size();
-		if (LSize < RSize || LSize == RSize
-			&& strcmp(left.c_str() + 1, right.c_str() + 1) < 0)
-		{
-			return "0";
-		}
-		else
-		{
-			if ("+1" == right || "-1" == right)
-			{
-				left[0] = symbol;
-				return left;
-			}
-		}
-
-		std::string strRet;
-		strRet.append(1, symbol);
-		char *pLeft = (char*)(left.c_str() + 1);
-		char *pRight = (char*)(right.c_str() + 1);
-		int DataLen = 1;
-		LSize -= 1;
-		// "2422222222"  33
-		for (int index = 0; index < LSize;)
-		{
-			if ('0' == *pLeft)
-			{
-				strRet.append(1, '0');
-				pLeft++;
-				index++;
-
-				continue;
-			}
-
-			if (!IsLeftStrBig(pLeft, DataLen, pRight, RSize - 1))
-			{
-				strRet.append(1, '0');
-				DataLen++;
-				if (index + DataLen > LSize)
-				{
-					break;
-				}
-				continue;
+				return BigData(_data + bigdata._data);
 			}
 			else
 			{
-				strRet.append(1, SubLoop(pLeft, DataLen, pRight, RSize - 1));
-
-				// pLeft
-				while ('0' == *pLeft && DataLen > 0)
+				if (('+' == s_data[0] && MAX_INT64 - _data >= bigdata._data)
+					|| ('-' == s_data[0] && INT_MIN + _data <= bigdata._data))
 				{
-					pLeft++;
-					index++;
-					DataLen--;
-				}
-
-				DataLen++;
-				if (index + DataLen > LSize)
-				{
-					break;
+					return BigData(_data + bigdata._data);
 				}
 			}
 		}
-
-		return strRet;
+		string strRet;
+		if (s_data[0] == bigdata.s_data[0])
+		{
+			strRet = Add(s_data, bigdata.s_data);
+		}
+		else
+		{
+			strRet = Sub(s_data, bigdata.s_data);
+		}
+		return BigData(strRet.c_str());
 	}
-
-	void INT64ToString()
-	{
-		//12345
-		char symbol = '+';
-		INT64 temp = m_llValue;
-		if (temp < 0)
-		{
-			symbol = '-';
-			temp = 0 - temp;
-		}
-
-		m_strData.append(1, symbol);
-		int count = 1;
-		// 54321
-		while (temp)
-		{
-			m_strData.append(1, temp % 10 + '0');
-			temp /= 10;
-		}
-
-		char *pLeft = (char*)(m_strData.c_str() + 1);
-		char *pRight = (char*)(m_strData.c_str() + m_strData.size() - 1);
-		while (pLeft < pRight)
-		{
-			char ctemp = *pLeft;
-			*pLeft++ = *pRight;
-			*pRight-- = ctemp;
-		}
-	}
+	BigData& operator-(const BigData& bigdata);
+	BigData& operator*(const BigData& bigdata);
+	BigData& operator/(const BigData& bigdata);
 
 	bool IsINT64Owerflow()const
 	{
-		std::string strTemp;
-		if ('+' == m_strData[0])
+		string strTmp;
+		if ('+' == s_data[0])
 		{
-			strTemp = "+9223372036854775807";
+			strTmp = "+9223372036854775807";
 		}
 		else
 		{
-			strTemp = "-9223372036854775808";
+			strTmp = "-9223372036854775808";
 		}
-
-		if (m_strData.size() > strTemp.size())
+		if (s_data.size() > strTmp.size())
 		{
 			return true;
 		}
-		else if (m_strData.size() == strTemp.size() && m_strData > strTemp)
+		else if (s_data.size() == strTmp.size() && s_data > strTmp)
 		{
 			return true;
 		}
-
-		return false;
-	}
-
-	bool IsLeftStrBig(char *pLeft, size_t LSize, char *pRight, size_t RSize)
-	{
-		assert(NULL != pLeft && NULL != pRight);
-		if (LSize > RSize || LSize == RSize 
-			&& strncmp(pLeft, pRight, LSize) >= 0)
+		else
 		{
-			return true;
+			return false;
 		}
-
-		return false;
 	}
-
-	char SubLoop(char *pLeft, size_t LSize, char *pRight, size_t RSize)
-	{
-		assert(NULL != pLeft && NULL != pRight);
-
-		char cRet = '0';
-		while (true)
-		{
-			if (!IsLeftStrBig(pLeft, LSize, pRight, RSize))
-			{
-				break;
-			}
-
-			// -=
-			int LDataLen = LSize - 1;
-			int RDataLen = RSize - 1;
-			while (RDataLen >= 0 && LDataLen >= 0)
-			{
-				if (pLeft[LDataLen] < pRight[RDataLen])
-				{
-					pLeft[LDataLen - 1] -= 1;
-					pLeft[LDataLen] += 10;
-				}
-
-				pLeft[LDataLen] = pLeft[LDataLen] - pRight[RDataLen] + '0';
-				LDataLen--;
-				RDataLen--;
-			}
-
-			// "990000000000000000000000000099"
-			while ('0' == *pLeft && LSize > 0)
-			{
-				pLeft++;
-				LSize--;
-			}
-
-			cRet++;
-		}
-
-		return cRet;
-	}
-
-
 private:
-	long long m_llValue;
-	std::string m_strData;
+	string Add(string left, string right)
+	{
+		int L_Size = left.size();
+		int R_Size = right.size();
+		if (L_Size < R_Size)//保证大的数在left的位置
+		{
+			swap(L_Size, R_Size);
+			swap(left, right);
+		}
+		string strRet;
+		char Step;
+
+		strRet.resize(L_Size + 1);//保证可以保存相加之后的字符串
+		strRet[0] = left[0];
+		Step = 0;                 //进位
+		for (int index = 1; index < L_Size; ++index)//第一位符号位保留，左对齐相加
+		{
+			char Ret = left[L_Size - index] - '0' + Step;
+
+			if (index < R_Size)
+			{
+				Ret += (right[R_Size - index] - '0');
+			}
+			strRet[L_Size - index + 1] = (Ret % 10 + '0');
+			Step = Ret / 10;
+		}
+		strRet[1] = (Step + '0');
+
+		return strRet;
+	}
+	string Sub(string left, string right)
+	{
+		int L_Size = left.size();
+		int R_Size = right.size();
+		if (L_Size < R_Size || (L_Size == R_Size && left < right))
+		{
+			swap(L_Size, R_Size);
+			std::swap(left, right);
+		}
+		string strRet;
+
+		strRet.resize(L_Size);
+		strRet[0] = left[0];
+		for (int index = 1; index < L_Size; index++)
+		{
+			char cRet = left[L_Size - index] - '0';
+			if (index < R_Size)
+			{
+				cRet += (right[R_Size - index] - '0');
+			}
+			if (cRet < 0)
+			{
+				left[L_Size - index - 1] -= 1;
+				cRet += 10;
+			}
+			strRet[L_Size - index] = (cRet + '0');
+		}
+		return strRet;
+	}
+	void INT64ToStirng()
+	{
+		char symbol = '+';
+		INT64 tmp = _data;
+		if (tmp < 0)
+		{
+			symbol = '-';
+			tmp = 0 - tmp;
+		}
+		s_data.append(1, symbol);
+		while (tmp >0)
+		{
+			s_data.append(1, tmp % 10 + '0');
+			tmp = tmp / 10;
+		}
+		char* left = (char*)(s_data.c_str() + 1);
+		char* right = (char*)(s_data.c_str() + s_data.size() - 1);
+		while (left < right)
+		{
+			char ctmp = *left;
+			*left++ = *right;
+			*right-- = ctmp;
+		}
+
+	}
+	bool IsLeftStrBig(char *pLeft, size_t LSize, char *pRight, size_t RSize);
+private:
+	INT64 _data;
+	string s_data;
 };
-BigData BigData::operator+(BigData& bigData)
-{
-	if (!IsINT64Owerflow() && !bigData.IsINT64Owerflow())
-	{
-		// '+' '-'||'-' '+'  7 + (-3)  10
-		if (m_strData[0] != bigData.m_strData[0])
-		{
-			return BigData(m_llValue + bigData.m_llValue);
-		}
-		else
-		{
-			// '+' '+' 3 + 7  10 - 7 >= 3
-			// '-' '-' -3 + (-7)  -10 - (-7) =< -3
-			if (('+' == m_strData[0] && MAX_INT64 - m_llValue >= bigData.m_llValue) ||
-				('-') == m_strData[0] && MIN_INT64 - m_llValue <= bigData.m_llValue)
-			{
-				return BigData(m_llValue + bigData.m_llValue);
-			}
-		}
-	}
-
-	std::string strRet;
-	if (m_strData[0] == bigData.m_strData[0])
-	{
-		strRet = Add(m_strData, bigData.m_strData);
-	}
-	else
-	{
-		strRet = Sub(m_strData, bigData.m_strData);
-	}
-
-	return BigData(strRet.c_str());
-}
-BigData BigData::operator-(const BigData& bigData)
-{
-	if (!IsINT64Owerflow() && !bigData.IsINT64Owerflow())
-	{
-		//7 - 3  -7 - (-3)
-		if (m_strData[0] == bigData.m_strData[0])
-		{
-			return BigData(m_llValue - bigData.m_llValue);
-		}
-		else
-		{
-			// '+' '-' 10 + (-7) >= 3
-			// '-' '+' -10 + 3 <= -7 
-			if (('+' == m_strData[0] && MAX_INT64 + bigData.m_llValue >= m_llValue) ||
-				('-' == m_strData[0] && MIN_INT64 + bigData.m_llValue <= m_llValue))
-			{
-				return BigData(m_llValue - bigData.m_llValue);
-			}
-		}
-	}
-
-	std::string strRet;
-	if (m_strData[0] != bigData.m_strData[0])
-	{
-		strRet = Add(m_strData, bigData.m_strData);
-	}
-	else
-	{
-		strRet = Sub(m_strData, bigData.m_strData);
-	}
-	return BigData(strRet.c_str());
-}
-BigData BigData::operator*(const BigData& bigData)
-{
-	if (0 == m_llValue || 0 == bigData.m_llValue)
-	{
-		return BigData(INT64(0));
-	}
-
-	if (!IsINT64Owerflow() && !bigData.IsINT64Owerflow())
-	{
-		if (m_strData[0] == bigData.m_strData[0])
-		{
-			//'+''+' 10 /2 = 5 >= 1 2 3 4 5
-			//'-''-' 10 /-2 = -5 <= -5 -4 -3 -2 -1 
-			if (('+' == m_strData[0] && MAX_INT64 / m_llValue >= bigData.m_llValue) ||
-				('-' == m_strData[0] && MAX_INT64 / m_llValue <= bigData.m_llValue))
-			{
-				return BigData(m_llValue*bigData.m_llValue);
-			}
-		}
-		else
-		{
-			//'+''-' -10 /2 = -5 <= -5 -4 -3 -2 -1
-			//'-''+' -10/-2 = 5 >= 5 4 3 2 1
-			if (('+' == m_strData[0] && MIN_INT64 / m_llValue <= bigData.m_llValue) ||
-				('-' == m_strData[0] && MIN_INT64 / m_llValue >= bigData.m_llValue))
-			{
-				return BigData(m_llValue*bigData.m_llValue);
-			}
-		}
-	}
-
-	return BigData(Mul(m_strData, bigData.m_strData).c_str());
-}
-BigData BigData::operator/(const BigData& bigData)
-{
-	if (0 == bigData.m_llValue)
-	{
-		assert("除数不能为0！");
-		return BigData(INT64(0));
-	}
-
-	if (!IsINT64Owerflow() && !bigData.IsINT64Owerflow())
-	{
-		return BigData(m_llValue / bigData.m_llValue);
-	}
-
-	return BigData(Div(m_strData, bigData.m_strData).c_str());
-}
